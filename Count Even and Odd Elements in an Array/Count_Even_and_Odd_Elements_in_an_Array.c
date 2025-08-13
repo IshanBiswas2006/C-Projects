60d01444f7954f03d627369f0fecd9a4913d4753
Ishan Biswas <biswas.ishan2006@gmail.com>
2025-08-13 19:50:22 +0530

    Add files via upload

---
+++ Count Even and Odd Elements in an Array/Count_Even_and_Odd_Elements_in_an_Array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arr[10],i,even=0,odd=0;
+
+    printf("Enter 10 numbers: ");
+    for (i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (i=0;i<10;i++)
+    {
+        if (arr[i]%2==0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        
+    }
+
+    printf("The even numbers: %d",even);
+    printf("The odd number: %d",odd);
+    
+    return 0;
+}
